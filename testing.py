4bfd7ec60932b256cecd20cb3918c56a486e89de
Cid Wilson Bellamy <142268723+CidWB@users.noreply.github.com>
2023-09-08 12:33:43 -0400

    Update testing.py
    I added a bit of edited code from a Mimo excercise.

--- testing.py
+++ testing.py
@@ -1,2 +1,13 @@
 # Adding a new file for Coursera practice
 print("Hello, world!")
+
+
+#Random Mimo code I edited
+def get_waiting_list(signups):
+  if signups > 200:
+    print(f"Waiting list: {signups - 200}")
+  elif signups == 200:
+    print("Waiting list full.")
+  else:
+    print(f"{200 - signups} spaces left.")
+get_waiting_list(20)
