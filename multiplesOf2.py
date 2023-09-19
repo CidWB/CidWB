0f5133c2d8aa6f08a963e54cb0fdccf4b19f55db
Cid Wilson Bellamy <142268723+CidWB@users.noreply.github.com>
2023-09-19 11:20:51 -0400

    Create multiplesOf2.py

---
+++ multiplesOf2.py
@@ -0,0 +1,8 @@
+#need to find a way to replicate this with c++
+
+count = 2
+multiple = 1
+while count <= 20:
+    print(f"2 by {multiple} is equal to {count}")
+    count += 2
+    multiple = count//2
